596dc5730d70531223afa466b3ee1f7eed09b6b8
vovnit-phie <vovnit@phie.ru>
2018-05-06 01:44:59 +0300

    add correct register for filter

--- lib/logstash/filters/rmf.rb
+++ lib/logstash/filters/rmf.rb
@@ -2,41 +2,55 @@
 require "logstash/filters/base"
 require "logstash/namespace"
 
-# This  filter will replace the contents of the default 
-# message field with whatever you specify in the configuration.
-#
-# It is only intended to be used as an .
 class LogStash::Filters::Rmf < LogStash::Filters::Base
 
-  # Setting the config_name here is required. This is how you
-  # configure this filter from your Logstash config.
-  #
-  # filter {
-  #    {
-  #     message => "My message..."
-  #   }
-  # }
-  #
   config_name "rmf"
-  
-  # Replace the message with this value.
-  config :message, :validate => :string, :default => "Hello World!"
+
+  config :whitelist, :validate => :list
   
 
   public
   def register
-    # Add instance variables 
+    # set whitelist to an array of arrays each element of those are field
+    # for example if we had ["a.a.a", "[b][c]"] we'll have [["a","a","a"]["b","c"]]
+    @whitelist.map! { |item| item.include? "[" ? item.split "][" : item.split "." }
+    @whitelist.each do |item|
+      if item.kind_of?(Array)
+        if item[0].include? "["
+          item[0]=item[1..-1]
+        end
+        if item[-1].include? "]"
+          item[-1]=item[0..-2]
+        end
+      end
+    end
   end # def register
+
+  private
+  def iterate(event, level, path)
+    
+    # if !event.is_a?(::Hash)
+    #   hash = event.to_hash
+    # else
+    #   hash = event
+    # end
+    # @whitelist.each do |allowed|
+    #   hash.each do |k,v|
+    #     if v.is_a?(::Hash)
+    #       iterate(v)
+    #     else
+    #       @whitelist.each do |field|
+    #
+    #       end
+    #     end
+    #   end
+    # end
+  end
 
   public
   def filter(event)
 
-    if @message
-      # Replace the event message with our message as configured in the
-      # config file.
-      event.set("message", @message)
-    end
-
+    iterate(event)
     # filter_matched should go in the last line of our successful code
     filter_matched(event)
   end # def filter
